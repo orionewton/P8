351636b5b5dd2cf1eb39f717a099efa0d3b3928e
Loan Tytgat <loantytgat@gmail.com>
2020-07-06 15:13:27 +0200

    update

--- users/views.py
+++ users/views.py
@@ -15,7 +15,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, 'Votre compte a bien été crée')
-            return redirect('catalog:index')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
@@ -23,6 +23,23 @@ def register(request):
 
 @login_required
 def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Les informations du profil ont été mises à jour.')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
     return render(request, 'users/profile.html')
 
 
